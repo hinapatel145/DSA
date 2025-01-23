0c64aa783c5129391c4133858eb4cd77a3270790
hina patel <hinapatel1452005@gmail.com>
2025-01-23 13:05:31 +0530

    add final code

--- ARRAY/LargestElement_01.java
+++ ARRAY/LargestElement_01.java
@@ -1,5 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class LargestElement_01{
+    public static int findLargest(int[] a){
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<a.length;i++){
+            if(a[i]>max){
+                max=a[i];
+            }
+        }
+        return max;
+    }
     public static void main(String[] args) {
-        
+        int[] a = {1,5,2,8,4,2};
+        findLargest(a);
+        System.out.println(findLargest(a));
+
     }
 }
